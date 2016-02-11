5dbd81600a5406ce335dfc1fa02de5080b6007a2
Xalandra <xalandra@gmail.com>
2016-02-11 14:33:54 +0100

    CMS van 11-2-16 van serverict

---
+++ cms/edit.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>UWV Wajong Vacatures</title>
+  <meta name="description" content="UWV Wajong Vacatures">
+  <meta name="author" content="Stenden Hogeschool">
+
+  <link rel="stylesheet" href="css/style.css">
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<?php
+
+require("common.php"); 
+     
+    if(empty($_SESSION['user'])) 
+    { 
+        header("Location: login.php"); 
+         
+        die("Redirecting to login.php"); 
+    } 
+
+include 'elements/header.php';
+include 'elements/vacancyedit.php';
+
+?>
+
+
+</body>
+</html>
